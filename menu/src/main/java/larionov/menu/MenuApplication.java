7e6a833311107603c801047c78b13783b273713f
OleksandrLarionov <larionov.alex23@gmail.com>
2024-01-09 21:13:45 +0100

    ordine dinamico

--- menu/src/main/java/larionov/menu/MenuApplication.java
+++ menu/src/main/java/larionov/menu/MenuApplication.java
@@ -14,53 +14,5 @@ public class MenuApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MenuApplication.class, args);
-
-        System.out.println("*******************************Info*******************************");
-
-
-
-/*        int choice;
-        do {
-            choice = welcome();
-        } while (choice != 4);
-
-        System.out.println("Arrivederci!");
-
+    }
 }
-    public static int welcome () {
-        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(MenuApplication.class);
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Benvenuto");
-        System.out.println("Questo è il nostro menu :D");
-        Menu menu = (Menu) ctx.getBean("getMenu");
-        System.out.println(menu);
-        System.out.println("E' pronto per ordinare?");
-
-        System.out.println("Cosa le porto?");
-
-        System.out.println("4. Esci");
-        int choice = scanner.nextInt();
-        scanner.nextLine();
-        switch (choice) {
-            case 1:
-                Pizza napoli = (Pizza) ctx.getBean("napoli");
-                scanner.nextLine();
-                break;
-            case 2:
-                System.out.println("xd");
-                break;
-            case 3:
-                System.out.println("*******************************************************");
-                break;
-            case 4:
-                scanner.close();
-                break;
-            default:
-                System.out.println("Scelta non valida. Riprova.");
-
-        }
-        ctx.close();
-        return choice;
-    }*/
-
-}}
--- menu/src/main/java/larionov/menu/entities/MyRunner.java
+++ menu/src/main/java/larionov/menu/entities/MyRunner.java
@@ -3,7 +3,10 @@ package larionov.menu.entities;
 import larionov.menu.MenuApplication;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+
+import java.util.Scanner;
 
 @Component
 public class MyRunner implements CommandLineRunner {
@@ -12,23 +15,179 @@ public class MyRunner implements CommandLineRunner {
     public void run(String... args) throws Exception {
         AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(MenuApplication.class);
 
-        Sala tavoliDisponibiliAlLocale = (Sala) ctx.getBean("getTavoliDisponibiliNellaSala");
+        System.out.println("Benvenuto Alla Pizzeria Grande Piramide Di Egitto");
+        int numeroCoperti = coperti();
+        welcome();
+        int choice = getUserChoise();
+        sceltaDelTavolo(choice);
+        mostraMenu();
+        prontoPerOrdinare();
+        Ordine ordine = ordineAlTavolo(sceltaDelTavolo(choice), numeroCoperti);
+        ordine.setCoperto(numeroCoperti);
+        ordine.totaleDellOrdine();
 
-        Tavolo tavoloSociale = (Tavolo) ctx.getBean("tavoloSociale") ;
-        tavoloSociale.setNumeroDiCopertiEffettivi(3);
-        tavoloSociale.setStatoDelTavolo(STATO.OCCUPATO);
 
-        Ordine nuovoOrdine = new Ordine(tavoloSociale);
-        nuovoOrdine.setNumeroDiCoperti(3);
-        System.out.println("****************" + nuovoOrdine.getNumeroDiCoperti());
+        System.out.println("Arrivederci");
+        ctx.close();
+    }
 
-        nuovoOrdine.aggiunguAllOrdine(ctx.getBean("napoli", Pizza.class));
-        nuovoOrdine.aggiunguAllOrdine(ctx.getBean("fanta", Bevande.class));
-        nuovoOrdine.aggiunguAllOrdine(ctx.getBean("prosciutto",Condimenti.class));
-        nuovoOrdine.setCoperto(ctx.getBean("getCoperto", double.class));
+    //    ***************************Metodi**********************************
+    private void welcome() {
+        System.out.println("Dove Desidera Sedersi?");
+        System.out.println("Seleziona un'opzione:");
+        System.out.println("0. Tavolo sociale");
+        System.out.println("1. Tavolo numero 1");
+        System.out.println("2. Tavolo numero 2");
+        System.out.println("3. Tavolo numero 3");
+        System.out.println("4. Tavolo numero 4");
+    }
 
-        System.out.println(tavoliDisponibiliAlLocale);
-        System.out.println(nuovoOrdine);
-        nuovoOrdine.totaleDellOrdine();
+    private int coperti(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("In Qianti siete?");
+        return Integer.parseInt(scanner.nextLine());
+    }
+
+    private void pizze() {
+        System.out.println("1. Napoli");
+        System.out.println("2. Margherita");
+        System.out.println("3. Pizza Al Salame");
+    }
+
+    private void bevande() {
+        System.out.println("1. CocaCola");
+        System.out.println("2. Fanta");
+        System.out.println("3. Linota");
+        System.out.println("4. Acqua");
+        System.out.println("5. Vino");
+    }
+
+    private int getUserChoise() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Inserisci la tua scelta: ");
+        return Integer.parseInt(scanner.nextLine());
+    }
+
+    private Tavolo sceltaDelTavolo(int choice) {
+        Tavolo tavoloAssegnato = null;
+        switch (choice) {
+            case 0:
+                tavoloAssegnato = assegnaIlTavolo("tavoloSociale");
+                break;
+            case 1:
+                tavoloAssegnato = assegnaIlTavolo("tavoloNumeroUno");
+                break;
+            case 2:
+                tavoloAssegnato = assegnaIlTavolo("tavoloNumeroDue");
+                break;
+            case 3:
+                tavoloAssegnato = assegnaIlTavolo("tavoloNumeroTre");
+                break;
+            case 4:
+                tavoloAssegnato = assegnaIlTavolo("tavoloNumeroQuattro");
+                break;
+
+            default:
+                System.out.println("Il tavolo non è disponibile");
+        }
+        return tavoloAssegnato;
+    }
+
+    private Tavolo assegnaIlTavolo(String nomeDelTavolo) {
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(MenuApplication.class);
+
+        Tavolo tavoloAssegnato = ctx.getBean(nomeDelTavolo, Tavolo.class);
+        tavoloAssegnato.setStatoDelTavolo(STATO.OCCUPATO);
+        return tavoloAssegnato;
+    }
+
+    private void mostraMenu() {
+        System.out.println("********************************LE NOSTRE PIZZE*********************************");
+        pizze();
+        System.out.println("********************************LE NOSTRE BEVANDE*********************************");
+        bevande();
+    }
+
+    private Bevande scegliLaBevanda(String bevanda) {
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(MenuApplication.class);
+        Bevande bevandaScelta = ctx.getBean(bevanda, Bevande.class);
+        return bevandaScelta;
+    }
+
+    private Pizza scegliLaPizza(String pizza) {
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(MenuApplication.class);
+        Pizza pizzaScelta = ctx.getBean(pizza, Pizza.class);
+        return pizzaScelta;
+    }
+
+    private void scegliIlCondimento(String condimento) {
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(MenuApplication.class);
+        Condimenti COndimentoScelto = ctx.getBean(condimento, Condimenti.class);
+    }
+
+    private void prontoPerOrdinare() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Pronti?");
+        if (scanner.nextLine().equalsIgnoreCase("si")) System.out.println("Cosa prendete?");
+        else do{
+            System.out.println("E ora!?");
+        } while (!scanner.nextLine().equalsIgnoreCase("si"));
+    }
+
+    private Pizza ordinePizze(int choice) {
+        Pizza pizza = null;
+        switch (choice) {
+            case 1:
+                pizza = scegliLaPizza("napoli");
+                break;
+            case 2:
+                pizza = scegliLaPizza("margherita");
+                break;
+            case 3:
+                pizza = scegliLaPizza("pizzaAlSalame");
+                break;
+            default:
+                System.out.println("Va a casa");
+        }
+        return pizza;
+    }
+
+    private Bevande ordineBevande(int choice) {
+        Bevande bevanda = null;
+        switch (choice) {
+            case 1:
+                bevanda = scegliLaBevanda("cocacola");
+                break;
+            case 2:
+                bevanda = scegliLaBevanda("fanta");
+                break;
+            case 3:
+                bevanda = scegliLaBevanda("lemonade");
+                break;
+            case 4:
+                bevanda = scegliLaBevanda("water");
+                break;
+            case 5:
+                bevanda = scegliLaBevanda("wine");
+                break;
+            default:
+                System.out.println("Va a casa");
+        }
+        return bevanda;
+    }
+
+    private Ordine ordineAlTavolo(Tavolo tavolo, int coperti) {
+        Ordine nuovoOrdine = new Ordine(tavolo);
+        for(int i=0; i<coperti; i++){
+            System.out.println("Che pizza vuole?");
+            int sceltaPizze = getUserChoise();
+            nuovoOrdine.aggiunguAllOrdine(ordinePizze(sceltaPizze));
+            System.out.println("Cosa le porto da bere invece?");
+            int sceltaBevande = getUserChoise();
+            nuovoOrdine.aggiunguAllOrdine(ordineBevande(sceltaBevande));
+        }
+        return nuovoOrdine;
     }
 }
+
